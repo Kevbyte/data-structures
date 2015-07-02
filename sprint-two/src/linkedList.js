cfb5b72eed6cb0818e9e210db2ffc4423c9019fb
Hack Reactor Students <accounts@hackreactor.com>
2015-07-02 16:05:48 -0700

    Completed tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+    }
+    if(list.tail !== null){
+      list.tail.next = newNode; 
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var formerHead = list.head;
+    list.head = list.head.next;
+    return formerHead.value;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    while(currentNode !== null) {
+      if(currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +47,5 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// head (4) 5 6 7 8 9 tail
