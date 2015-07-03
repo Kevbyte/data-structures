d256b0d1e205efa5fcf1157c598c037515907e56
Hack Reactor Students <accounts@hackreactor.com>
2015-07-03 13:17:47 -0700

    Passed tests for tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,8 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,14 +15,32 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+		this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+	var result = false;
+	var searchChildren = function(tree) {
+		if(tree.value === target) {
+			result = true;
+		}
+		if(tree.children.length !== 0) {
+			for(var i=0; i<tree.children.length; i++){
+				searchChildren(tree.children[i]);
+			}
+		}
+	}
+	searchChildren(this);
+	return result;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ var tree = {
+ 	value: 5, 
+ 	children: [{value: 6, children: [{value: 7, children: []}, {value: 8, children: []}]}]
+ }
+
