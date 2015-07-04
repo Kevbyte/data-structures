87ca0664cd28ee330fa1579307252afcdb88db1c
Hack Reactor Students <accounts@hackreactor.com>
2015-07-04 10:00:39 -0700

    Passed all tests for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,28 +5,32 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit); //<----this is the hash function
-  // var bucket = this._storage.get(i)
-   if(this._storage.get(i)===undefined){
-			this._storage.get(i) = [];   
-   }
-   console.log("hi")
-   this._storage.set(i, [k, v]);
-   console.log(this._storage)
+ 	if(!this._storage.get(i)){
+ 		this._storage.set(i, []);
+ 	}
 
-  
+ 	this._storage.get(i).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // this._storage.get(i).each(function(pair){
-  // 	if(pair[0]===k){
-  // 		return pair[1];
-  // 	}
-  // });
+  var result = null;
+  _.each(this._storage.get(i), function(pair){
+  	if(pair[0]===k){
+  		result = pair[1];
+  	}
+  });
+
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
-
+	var i = getIndexBelowMaxForKey(k, this._limit);
+	_.each(this._storage.get(i), function(pair){
+		if(pair[0]===k){
+			pair[1]=null;
+		}
+	});
 };
 
 
