8b3833095ca1232aa1ffa0c80fe894f48ab14b45
Hack Reactor Students <accounts@hackreactor.com>
2015-07-04 16:43:14 -0700

    Finished Set Working on Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,67 @@
 
 
 var Graph = function(){
+  this.storage = {};  //{node: {key:[]}}
 };
 
 Graph.prototype.addNode = function(node){
+  //takes a new node and adds it to graph puppies = {edges: []}
+  this.storage[node] = {edges:[]};
 };
 
 Graph.prototype.contains = function(node){
+  //takes any node and returns a boolean if it exists in graph
+  var result = false;
+  if(this.storage[node]) {
+    result = true;
+  }
+  return result;
 };
 
 Graph.prototype.removeNode = function(node){
+  //takes a node and removes it
+  delete this.storage[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  //returns a boolean based on whether or not two nodes are connected
+  //if puppies's edges has kittens in the array and if kittens's edges has puppies in the array
+  var result = false;
+  for(var i = 0; i < this.storage[fromNode].edges.length; i++) {
+    if(this.storage[fromNode].edges[i] === toNode) {
+      result = true;
+    }
+  }
+  return result;
 };
 
-Graph.prototype.addEdge = function(fromNode, toNode){
+Graph.prototype.addEdge = function(fromNode, toNode){ //puppies, kittens
+  //creates a connection between two nodes in graph
+  this.storage[fromNode].edges.push(toNode);
+  this.storage[toNode].edges.push(fromNode);
+  // console.log(this.storage[fromNode].edges)
+  // console.log(this.storage[toNode].edges)
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  //removes a connection between two nodes
+  for(var i = 0; i < this.storage[fromNode].edges.length; i++) {
+    if(this.storage[fromNode].edges[i] === toNode) {
+      this.storage[fromNode].edges.splice(i, 1);
+    }
+  }
+  for(var i = 0; i < this.storage[toNode].edges.length; i++) {
+    if(this.storage[toNode].edges[i] === fromNode) {
+      this.storage[toNode].edges.splice(i, 1);
+    }
+  }
 };
 
 Graph.prototype.forEachNode = function(cb){
+  //traverses through graph, calling the passed in function once on each node
+  _.each(this.storage, function(value){
+    cb(value);
+  });
 };
 
 /*
@@ -29,4 +69,4 @@ Graph.prototype.forEachNode = function(cb){
  */
 
 
-
+// myGraph = new Graph
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,45 @@ var Set = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  //adding values to a set(an array)
+  var result = false;
+  if(!this._storage){
+    this._storage = [];
+  }
+  _.each(this._storage, function(value){ //look at each index in array
+    if(value === item){                   // if current value is item update result
+      result = true;
+    }
+  });
+  if(result===false){
+    this._storage.push(item);
+  }
+
+  
 };
 
 setPrototype.contains = function(item){
+  //need to look at all items in the set
+  var result = false;
+  _.each(this._storage, function(value){
+    if(value===item){ //if the item in the set is equal to the item we are looking for
+      result = true;    //set result equal to true
+    }
+  });
+  return result;      //return result
 };
 
 setPrototype.remove = function(item){
+  //loop through array and if we ever find item in array splice it
+  for(var i=0; i<this._storage.length; i++){
+    if(this._storage[i]===item){
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ ["Mel Gibson", "adam"]
